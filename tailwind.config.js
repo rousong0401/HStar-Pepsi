a97eccae57eff2f86592372a0e148cdc939ed6cc
rousong <recall131477@gmail.com>
2021-06-15 11:31:02 +0800

    圖片測試

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
-    enabled: true,
-    content: ['html/*.html', 'js/*.js'],
+    // enabled: true,
+    // content: ['html/*.html', 'js/*.js'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
